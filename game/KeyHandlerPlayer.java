54d4b82939d1485aab544553004cfd1abcf252ce
Jonas Knopf <jonas.knopf06@gmail.com>
2023-06-11 22:59:53 +0200

    KeyHandlerPlayer erstellt (Test)

---
+++ game/KeyHandlerPlayer.java
@@ -0,0 +1,22 @@
+package game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandlerPlayer implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
